32fef6de0c71f453abbd1a9e1df021ebbb6e3be0
concs-niemeyer <conrado.raizen@gmail.com>
2024-10-14 21:44:58 -0300

    versão final auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,28 +4,31 @@ async function auth(req, res, next) {
   try {
     console.log("Entramos no Middleware auth.js");
 
-    // Pegando o cabeçalho Authorization
-    const token = localStorage.getItem('token')
-	console.log("token",token)
-    
-	// Verifica se o cabeçalho Authorization existe e se está no formato correto
-    if (!token) {
+    // Pegando o cabeçalho Authorization enviado pelo frontend
+    const authHeader = req.headers.authorization;
+
+    // Verifica se o cabeçalho Authorization existe e se está no formato correto
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({
         error: "Autenticação Falhou!",
         cause: "Token não encontrado ou mal formatado",
       });
     }
 
+    // Extrai o token do cabeçalho Authorization
+    const token = authHeader.split(" ")[1];
+    console.log("token", token);
+
     // Verificar o token usando jwt.verify
     req.payload = verify(token, process.env.SECRET_JWT);
-	console.log(req.payload)
+    console.log(req.payload);
 
     // Se tudo estiver correto, continuar para o próximo middleware
     next();
   } catch (error) {
     // Tratamento de erros específicos do JWT
     let message = "Token inválido ou expirado";
-    
+
     if (error.name === "TokenExpiredError") {
       message = "Token expirado";
     } else if (error.name === "JsonWebTokenError") {
